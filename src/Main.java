0e7e5873d92eff38e95644f61605eb4a67c7c067
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-05-14 10:35:45 +0900

    変更したファイルをアップロード

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-public class Review1 {
+public class Main {
     public static void main(String[] args) {
-        System.out.print("山下　留奈");
-        System.out.println("今日の天気は" + "晴れです。");
+        System.out.println("私の名前は山下留奈です。");
+        System.out.println("システムに変更を加えた");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+            System.out.println("i = " + i);
+        }
     }
 }
