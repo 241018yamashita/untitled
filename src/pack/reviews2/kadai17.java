1db3580a7a2d7b87ed588ae291b98e09abccab1b
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-05-26 11:36:06 +0900

    練習問題16

---
+++ src/pack/reviews2/kadai17.java
@@ -0,0 +1,14 @@
+package pack.reviews2;
+
+public class kadai17 {
+    public static void main(String[] args) {
+        int num = 20;
+        int sum = 0;
+        int i = 2;
+        while (i < num) {
+            sum = sum + i;
+            i = i + 2;
+        }
+        System.out.println("結果: " + sum);
+    }
+}
