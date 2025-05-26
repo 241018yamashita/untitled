aa24bbd191647f3af26b667bb10ec2ca04106038
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-05-26 11:14:23 +0900

    練習問題14

---
+++ src/pack/reviews2/kadai15.java
@@ -0,0 +1,14 @@
+package pack.reviews2;
+
+public class kadai15 {
+    public static void main(String[] args) {
+        int num = 1;
+        if (num % 2 == 0) {
+            System.out.println("整数");
+        } else if (num % 2 <= num) {
+            System.out.println("奇数");
+        } else {
+            System.out.println("無効な入力です。");
+        }
+    }
+}
