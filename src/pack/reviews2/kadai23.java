debe22066635c3a6dfea315dcb89b065237c0f92
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-05-26 12:08:40 +0900

    練習問題23

---
+++ src/pack/reviews2/kadai23.java
@@ -0,0 +1,12 @@
+package pack.reviews2;
+
+public class kadai23 {
+    public static void main(String[] args) {
+        int num = 10;
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("結果: " + factorial);
+    }
+}
