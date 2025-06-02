3c72552b5c6531db4750d093832ddfa8ce53f1bc
241018yamashita <241018@std.hi-joho.ac.jp>
2025-06-02 10:38:50 +0900

    練習問題27

---
+++ src/pack/reviews2/kadai27.java
@@ -0,0 +1,15 @@
+package pack.reviews2;
+
+public class kadai27 {
+    public static void main(String[] args) {
+        hello();
+        display("World");
+    }
+    public static void display(String msg) {
+        System.out.println("msg");
+        System.out.println(msg);
+    }
+    public static void hello() {
+        System.out.println("Hello");
+    }
+}
