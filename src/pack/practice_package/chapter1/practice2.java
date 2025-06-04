d937038a452376697b51456d740c96e4c951ce31
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-06-04 09:47:00 +0900

    追加点課題

---
+++ src/pack/practice_package/chapter1/practice2.java
@@ -0,0 +1,13 @@
+package pack.practice_package.chapter1;
+
+public class practice2 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a = 3;
+        b = 5;
+        c = a * b;
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    }
+}
