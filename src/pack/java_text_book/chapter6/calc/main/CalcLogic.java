153dd13a4fe3a5cedb628499eead3b9872cc6ef2
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-06-04 09:19:38 +0900

    授業資料1

---
+++ src/pack/java_text_book/chapter6/calc/main/CalcLogic.java
@@ -0,0 +1,10 @@
+package pack.java_text_book.chapter6.calc.main;
+
+public class CalcLogic {
+    public static int tasu(int a, int b) {
+        return (a + b);
+    }
+    public static int hiku(int a, int b) {
+        return (a - b);
+    }
+}
