4933b116ca3085601142b7297eca48468e2d559d
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-06-11 09:00:46 +0900

    追加点課題

---
+++ src/pack/practice_package/chapter3/practice4.java
@@ -0,0 +1,12 @@
+package pack.practice_package.chapter3;
+
+public class practice4 {
+    public static void main(String[] args) {
+        boolean tenki = true;
+        if (tenki == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+
+        }
+    }
+}
