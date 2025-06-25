6d2835bbb2d5829b4c08b2a4a42754e6b507f284
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-06-25 10:36:56 +0900

    追加点課題

---
+++ src/pack/practice_package/chapter5/practice1.java
@@ -0,0 +1,17 @@
+package pack.practice_package.chapter5;
+
+public class practice1 {
+    public static void main(String[] args) {
+        introduceOneself();
+    }
+    public static void introduceOneself() {
+        String name = "湊雄輔";
+        int age = 22;
+        double height = 169.9;
+        char zodiac = '辰';
+        System.out.print("私の名前は" + name + "です。");
+        System.out.print("歳は" + age + "です。");
+        System.out.println("身長は" + height + "cmです。");
+        System.out.println("十二支は" + zodiac + "です。");
+    }
+}
