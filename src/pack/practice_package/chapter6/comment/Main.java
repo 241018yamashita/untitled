8cfefe1d75925a63c2daaece95cf9b255b88e3dc
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-07-02 08:53:11 +0900

    追加点課題 practice1・3

---
+++ src/pack/practice_package/chapter6/comment/Main.java
@@ -0,0 +1,10 @@
+package pack.practice_package.chapter6.comment;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Zenhan.doWarusa();
+        Zenhan.doTogame();
+        pack.practice_package.chapter6.comment.Kouhan.callDeae();
+        pack.practice_package.chapter6.comment.Kouhan.showMondokoro();
+    }
+}
